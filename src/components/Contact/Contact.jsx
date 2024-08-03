a5c0b5e91fd46e40d6013ea9ba027fbbf2550f59
Olha-Rizhko <152223653+Olha-Rizhko@users.noreply.github.com>
2024-08-03 17:10:14 +0200

    init

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+// import { FaUserLarge, FaPhone } from "react-icons/fa6";
+import { FaUser, FaPhoneAlt } from "react-icons/fa";
+
+import css from "./Contact.module.css";
+
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
+
+export default function Contact({ contactInfo: { id, name, number } }) {
+  const dispatch = useDispatch();
+
+  const onDelete = () => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <div className={css.item}>
+      <div>
+        <div className={css.contactTitle}>
+          <FaUser className={css.contactIcon} />
+          <p className={css.contactInfo}>{name}</p>
+        </div>
+
+        <div className={css.contactTitle}>
+          <FaPhoneAlt className={css.contactIcon} />
+          <p className={css.contactInfo}>{number}</p>
+        </div>
+      </div>
+
+      <button type="button" className={css.button} onClick={onDelete}>
+        Delete
+      </button>
+    </div>
+  );
+}
